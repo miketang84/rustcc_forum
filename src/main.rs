c6ce9ff0371e5cd4a3c90f7caebaaf131e199119
Daogang Tang <daogangtang@gmail.com>
2023-04-28 16:57:26 +0800

    wip.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+
+    let app = Router::new()
+        .route("/foo", get(|| async { "Hi from /foo" }))
+        .nest_service("/assets", serve_dir.clone())
+        .fallback_service(serve_dir);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }

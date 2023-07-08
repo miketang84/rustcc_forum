f61ffabaa4fc314674c7ff2b96bc7f687fe8da76
Daogang Tang <daogangtang@gmail.com>
2023-07-09 03:31:51 +0800

    wip.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/comment.rs
+++ src/comment.rs
@@ -2,10 +2,12 @@ use askama::Template;
 use axum::{
     extract::{Form, Query, RawQuery, State},
     response::{Html, IntoResponse, Redirect},
+    Extension,
 };
-use gutp_types::{GutpComment, GutpPost};
+use gutp_types::{GutpComment, GutpPost, GutpUser};
+use serde::{Deserialize, Serialize};
 
-use crate::Client;
+use crate::redirect_to_error_page;
 use crate::HtmlTemplate;
 use crate::{make_get, make_post};
 
@@ -20,59 +22,91 @@ struct ViewCommentCreateParams {
 }
 
 pub async fn view_comment_create(
-    State(client): State<Client>,
+    Extension(logged_user_id): Extension<Option<String>>,
     Query(params): Query<ViewCommentCreateParams>,
-    RawQuery(query): RawQuery,
 ) -> impl IntoResponse {
     // check the user login status
+    if logged_user_id.is_none() {
+        let action = format!("Not logged in");
+        let err_info = "Need login firstly to get proper permission.";
+        return redirect_to_error_page(&action, err_info).into_response();
+    }
 
-    // We must specify the subspace_id
-    let post_id = params.post_id;
-
-    // get the post object from the gutp service, to check the exsistence of that post
-    let query = format!("id={}", post_id);
-    let res_bytes = make_get(client, "/v1/post", Some(query)).await;
-    let posts: Vec<GutpPost> = serde_json::from_slice(res_bytes).unwrap_or(vec![]);
+    let inner_params = [("id", &params.post_id)];
+    let posts: Vec<GutpPost> = make_get("/v1/post", &inner_params).await.unwrap_or(vec![]);
     if let Some(post) = posts.into_iter().next() {
-        // render the page
-        HtmlTemplate(CommentCreateTemplate { post })
+        HtmlTemplate(CommentCreateTemplate { post }).into_response()
     } else {
-        let action = format!("Query Article: {}", post_id);
+        let action = format!("Query Article: {}", &params.post_id);
         let err_info = "Article doesn't exist, comment couldn't be added to it!";
-        let redirect_uri = format!("/error/info?action={}&err_info={}", action, err_info);
-        Redirect::to(&redirect_uri)
+        redirect_to_error_page(&action, err_info).into_response()
     }
 }
 
 struct PostCommentCreateParams {
     post_id: String,
+    content: String,
 }
 
 pub async fn post_comment_create(
-    State(client): State<Client>,
+    Extension(logged_user_id): Extension<Option<String>>,
     Form(params): Form<PostCommentCreateParams>,
-    body: String,
 ) -> impl IntoResponse {
     // check the user login status
+    if logged_user_id.is_none() {
+        let action = format!("Not logged in");
+        let err_info = "Need login firstly to get proper permission.";
+        return redirect_to_error_page(&action, err_info);
+    }
 
-    // TODO: parse form params
-    let post_id = params.post_id;
+    let inner_params = [("id", &params.post_id)];
+    let posts: Vec<GutpPost> = make_get("/v1/post", &inner_params).await.unwrap_or(vec![]);
+    if let Some(post) = posts.into_iter().next() {
+        // retreive author info
+        let inner_params = [("id", &logged_user_id.clone().unwrap())];
+        let authors: Vec<GutpUser> = make_get("/v1/user", &inner_params).await.unwrap_or(vec![]);
+        if let Some(author) = authors.into_iter().next() {
+            #[derive(Serialize)]
+            struct InnerCommentCreateParams {
+                content: String,
+                author_id: String,
+                author_nickname: String,
+                post_id: String,
+                parent_comment_id: String,
+                is_public: bool,
+            }
 
-    // TODO: check the existence of article by query
+            let inner_params = InnerCommentCreateParams {
+                content: params.content.to_owned(),
+                author_id: author.id.to_owned(),
+                author_nickname: author.nickname.to_owned(),
+                post_id: post.id.to_owned(),
+                parent_comment_id: "".to_owned(),
+                is_public: true,
+            };
 
-    // forward post form body to gutp
-    let res_bytes = make_post(client, "/v1/comment/create", body).await;
-    let comments: Vec<GutpComment> = serde_json::from_slice(res_bytes).unwrap_or(vec![]);
-    if let Some(comment) = comments.into_iter().next() {
-        // redirect to the article page
-        let redirect_uri = format!("/article?id={}", comment.post_id);
-        Redirect::to(&redirect_uri)
+            let comments: Vec<GutpComment> = make_post("/v1/comment/create", &inner_params)
+                .await
+                .unwrap_or(vec![]);
+            if let Some(comment) = comments.into_iter().next() {
+                // redirect to the article page
+                let redirect_uri = format!("/article?id={}", comment.post_id);
+                Redirect::to(&redirect_uri)
+            } else {
+                // redirect to the error page
+                let action = format!("Create comment for article: {}", &post.id);
+                let err_info = "Unknown";
+                redirect_to_error_page(&action, err_info)
+            }
+        } else {
+            let action = format!("Query author: {}", &logged_user_id.clone().unwrap());
+            let err_info = "Unknown";
+            redirect_to_error_page(&action, err_info)
+        }
     } else {
-        // redirect to the error page
-        let action = format!("Create comment for article: {}", post_id);
-        let err_info = "Unknown";
-        let redirect_uri = format!("/error/info?action={}&err_info={}", action, err_info);
-        Redirect::to(&redirect_uri)
+        let action = format!("Query Article: {}", &params.post_id);
+        let err_info = "Article doesn't exist, comment couldn't be added to it!";
+        redirect_to_error_page(&action, err_info)
     }
 }
 
@@ -87,26 +121,26 @@ struct ViewCommentDeleteParams {
 }
 
 pub async fn view_comment_delete(
-    State(client): State<Client>,
+    Extension(logged_user_id): Extension<Option<String>>,
     Query(params): Query<ViewCommentDeleteParams>,
-    RawQuery(query): RawQuery,
 ) -> impl IntoResponse {
     // check the user login status
+    if logged_user_id.is_none() {
+        let action = format!("Not logged in");
+        let err_info = "Need login firstly to get proper permission.";
+        return redirect_to_error_page(&action, err_info).into_response();
+    }
 
-    // We must specify the tag_id, We can do it in the params type definition
-    let id = params.id;
-
-    // get the old tag by request to gutp
-    let res_bytes = make_get(client, "/v1/comment", query).await;
-    let comments: Vec<GutpComment> = serde_json::from_slice(res_bytes).unwrap_or(vec![]);
+    let inner_params = [("id", &params.id)];
+    let comments: Vec<GutpComment> = make_get("/v1/comment", &inner_params)
+        .await
+        .unwrap_or(vec![]);
     if let Some(comment) = comments.into_iter().next() {
-        HtmlTemplate(CommentDeleteTemplate { comment })
+        HtmlTemplate(CommentDeleteTemplate { comment }).into_response()
     } else {
-        // redirect to the error page
-        let action = format!("Query comment: {}", id);
-        let err_info = "This comment doesn't exist!";
-        let redirect_uri = format!("/error/info?action={}&err_info={}", action, err_info);
-        Redirect::to(&redirect_uri)
+        let action = format!("Query Article: {}", &params.id);
+        let err_info = "Article doesn't exist, comment couldn't be added to it!";
+        redirect_to_error_page(&action, err_info).into_response()
     }
 }
 
@@ -116,22 +150,23 @@ struct PostCommentDeleteParams {
 }
 
 pub async fn post_comment_delete(
-    State(client): State<Client>,
+    Extension(logged_user_id): Extension<Option<String>>,
     Form(params): Form<PostCommentDeleteParams>,
-    body: String,
 ) -> Redirect {
     // check the user login status
+    if logged_user_id.is_none() {
+        let action = format!("Not logged in");
+        let err_info = "Need login firstly to get proper permission.";
+        return redirect_to_error_page(&action, err_info);
+    }
 
-    // We must precheck the tag_id, we can do it in the params type definition
-    let comment_id = params.id;
-    let post_id = params.post_id;
-
-    // check whether some articles attaching to this tag
-    let res_bytes = make_post(client, "/v1/comment/delete", body).await;
-    let _comments: Vec<GutpComment> = serde_json::from_slice(res_bytes).unwrap_or(vec![]);
+    let inner_params = [("id", &params.id)];
+    let _comments: Vec<GutpComment> = make_get("/v1/comment/delete", &inner_params)
+        .await
+        .unwrap_or(vec![]);
 
     // TODO: process the error branch of deleting
 
-    let redirect_uri = format!("/article?id={}", post_id);
+    let redirect_uri = format!("/article?id={}", &params.post_id);
     Redirect::to(&redirect_uri)
 }
